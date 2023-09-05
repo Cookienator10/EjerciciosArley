fd5c79b68a18bf281e86d77de4fe87d9e095985d
Santiago Rojas Rodríguez <santiagorojas772@gmail.com>
2023-09-05 07:22:15 -0500

    se creo la carpeta ejercicis javascript  con los archivos respectivos.

---
+++ ejercicios java script/universidad/universidad.js
@@ -0,0 +1,13 @@
+// Construya un algoritmo con JavaScript” para las estadísticas de atención de una
+// universidad teniendo en cuenta los siguientes requisitos:
+// 1. Hay dos módulos de atención: terminal para llamada telefónica y oficina.
+// 2. El sistema brinda las estadísticas de todo el proceso de atención:
+// • Cantidad de usuarios atendidos.
+// • Atendidos por día y especificación por segmento (Estudiante – docente) en
+// cada uno de los módulos de atención.
+// • Se permite trasferir de módulo de atención y se debe generar estadística de
+// esta trasferencia.
+
+
+let maestro =[]
+let alumno =[]
